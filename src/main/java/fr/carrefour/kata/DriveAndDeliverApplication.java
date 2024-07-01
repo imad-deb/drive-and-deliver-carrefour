ef7557f8da938a2512ebadab931a3a7256f00465
ET34492 <imad.debbour@bred.fr>
2024-07-01 10:08:01 +0200

    initial commit

---
+++ src/main/java/fr/carrefour/kata/DriveAndDeliverApplication.java
@@ -0,0 +1,13 @@
+package fr.carrefour.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveAndDeliverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveAndDeliverApplication.class, args);
+    }
+
+}
